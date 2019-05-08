6cbac583cc3e04b5f1edb48892e49aa9819ce24f
Paul Smee <paulsmee.is@gmail.com>
2019-05-08 15:02:23 +1000

    specific record pick

--- db.js
+++ db.js
@@ -4,12 +4,12 @@ var db = new sqlite3.Database('test.db')
 
 const tempstats = {}
 
-db.get('SELECT 3 AS id , temp FROM temps', function(err, row){
+db.get('SELECT * FROM temps WHERE rowid = 3', function(err, row){
     if(err){
         console.log('Oh no!' + err.message);
     }
     else{
-        console.log('Row ID: ' + row.id + " shows a temperature of: " + row.temp +"c")
+        console.log('Row ID: ' + row._id + " shows a temperature of: " + row.temp +"c")
         tempstats.currentTemp = row.temp;
     }
 })
