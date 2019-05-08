357c85da5a3ebc4b056dced880f1260e66fa65ce
Paul Smee <paulsmee.is@gmail.com>
2019-05-08 10:15:55 +1000

    redesign

---
+++ db.js
@@ -0,0 +1,27 @@
+'use strict'
+var sqlite3 = require('sqlite3')
+var db = new sqlite3.Database('test.db')
+
+const tempstats = {}
+
+db.get('SELECT 2 AS id , temp FROM temps', function(err, row){
+    if(err){
+        console.log('Oh no!' + err.message);
+    }
+    else{
+        console.log('Row ID: ' + row.id + " shows a temperature of: " + row.temp +"c")
+        tempstats.currentTemp = row.temp;
+    }
+})
+
+db.get('SELECT * FROM temps WHERE date = DATETIME("now", "start of day", "localtime")', function(err, row){
+    if(err){
+        console.log('Oh no!' + err.message);
+    }
+    else{
+        console.log('Row ID: ' + row.id + " shows a temperature of: " + row.temp +"c")
+        tempstats.currentTemp = row.temp;
+    }
+})
+
+module.exports = tempstats
---
+++ router.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// This file will handle all the routing for the server
+
+var express = require('express')
+var router = express.Router()
+var tempstats = require('./db.js')
+
+router.use(express.urlencoded())
+router.use(express.json())
+
+router.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+  console.log("Access Detected!")
+})
+
+router.get('/view', function(req, res) {
+  res.send(tempstats.currentTemp+'')
+})
+
+module.exports = router
