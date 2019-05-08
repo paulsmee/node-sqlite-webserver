aa09ad5ab4baf1423fc9cc88a7dcb4679f5e700d
Paul Smee <paulsmee.is@gmail.com>
2019-05-08 10:23:57 +1000

    minor changes

--- db.js
+++ db.js
@@ -14,14 +14,14 @@ db.get('SELECT 2 AS id , temp FROM temps', function(err, row){
     }
 })
 
-db.get('SELECT * FROM temps WHERE date = DATETIME("now", "start of day", "localtime")', function(err, row){
-    if(err){
-        console.log('Oh no!' + err.message);
-    }
-    else{
-        console.log('Row ID: ' + row.id + " shows a temperature of: " + row.temp +"c")
-        tempstats.currentTemp = row.temp;
-    }
-})
+// db.get('SELECT * FROM temps WHERE date = DATETIME("now", "start of day", "localtime")', function(err, row){
+//     if(err){
+//         console.log('Oh no!' + err.message);
+//     }
+//     else{
+//         console.log('Row ID: ' + row.id + " shows a temperature of: " + row.temp +"c")
+//         tempstats.currentTemp = row.temp;
+//     }
+// })
 
 module.exports = tempstats
